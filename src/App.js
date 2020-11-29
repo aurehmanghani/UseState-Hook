4900332b39a0c034ae53d6c7b50f21cc44f9abf0
Atiq Ur Rehman <aurehmanghani@gmail.com>
2020-11-29 10:05:04 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UseEffectTest from './UseEffectTest';
+function App() {
+  return (
+    <div className="App">
+      <UseEffectTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UseEffectTest.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+
+const UseEffectTest = () => {
+  const [count,setCount] = useState(0)
+  const [age,setAge] = useState(0)
+  const [todos,setTodos] = useState([])
+  const [greetings, setGreetings] = useState('')
+  
+
+  return (
+    <div>
+    <p>Number of clicks : {count}</p>
+    <button onClick={()=>setCount(count+1)}>
+    Counter
+    </button>
+    <hr/>
+    <p>Set Age : {age}</p>
+    <button onClick={()=>setAge(38)}>
+    Counter
+    </button>
+     <hr/>
+    <p>Todos : {todos.todo}</p>
+    <button onClick={()=>setTodos({todo:"Buy Milk",complete:true})}>
+    Counter
+    </button>
+    <hr/>
+    <p>Greetings : {greetings}</p>
+    <input type="text" value={greetings} onChange={event => setGreetings(event.target.value)}/>
+    </div>
+  );
+}
+
+export default UseEffectTest;
